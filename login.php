ab8a2fdec55d50071cd54b8dcfab2ae67ec0fa41
Armindo86 <79296874+Armindo86@users.noreply.github.com>
2022-01-13 16:40:49 -0300

    Create login.php

---
+++ login.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    <link href = "https: //fonts.googleapis.com/css2? family = Roboto: wght @ 100 & display = swap "rel =" stylesheet ">
+    <link href="style.css" rel="stylesheet">
+    <title>Compostagem</title>
+</head>
+<body>
+    <section class="topo">
+        <div class="center">
+            <header>
+                 <div class="logo"><img src="images/logo.png"  /></div>
+            </header>
+            <div class="clear"></div>
+            <br />
+            <br />
+            <div class="w50 time-descricao">
+                <h2>Use o sistema para cadastro de composto</h2>
+                <a target="blank" href="https://www.in.gov.br/web/dou/-/instrucao-normativa-n-61-de-8-de-julho-de-2020-266802148">ver a legislação</a>
+            </div>
+            <div class="w50 time-imagem">
+                <img src="images/ilustracao.png" />
+            </div>
+            <div class="clear"></div>
+        <div>
+    </section>  
+
+        <?php
+        //inicialmente, criamos um vetor armazenando o preço de cada mercadoria disponível no carrinho de compras, usando o nome da mercadoria como índice associativo
+        $Login = $_POST['login'];
+        $Senha = $_POST['senha'];
+
+
+       
+         echo "<span>O login é $Login e a Senha:<span>$Senha</span>";
+    <?
+    <section class="diferenciais">
+        <div class="center">
+            <h2>Contribuindo de ponta a ponta</h2>
+                <div class="icons-diferenciais">
+                    <div class="box-single-diferencias">
+                        <img src="images/icon1.png" />
+                        <h3>Ambiente Mobile</h3>
+                        <p>Garanta que toda sua comunicaçãoesteja alinhada com seu proposito</p>
+                    </div>
+                    <div class="box-single-diferencias">
+                        <img src="images/icon2.png" />
+                        <h3>Ambiente Mobile</h3>
+                        <p>Garanta que toda sua comunicaçãoesteja alinhada com seu proposito</p>
+                    </div>
+                    <div class="box-single-diferencias">
+                        <img src="images/icon3.png" />
+                        <h3>Ambiente Mobile</h3>
+                        <p>Garanta que toda sua comunicaçãoesteja alinhada com seu proposito</p>
+                    </div>
+            </div>
+        </div>
+    </section>
+
+
+    </body>
+    </html>
